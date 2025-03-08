a9211f048552f058e18775b70b0cdd5a26005ae1
Binhanninza <143570394+Binhanninza@users.noreply.github.com>
2025-03-08 12:50:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     titleWeb: "Website tỏ tình",
     introTitle: 'Gửi người nhận được tin nhắn này <3 ',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Thư dành chonhững bạn nữ trong lớp 8A3 <33
+    `,
     btnIntro: '^^ ỏoooi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Chúc cậu có một ngày 8tháng 3 🥰',
+    desc: 'Thật vui vẻ, luôn là cô gái xinh đẹp, thông minh và "siêu đỉnh" như hôm nay và đừng quên giữ phong độ đấy nhé! Chúc bạn ngày 8/3 hạnh phúc. Có nhận lời chúc  này của bảnh khong??',
+    btnYes: 'Oce lun <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Tớ biết mà <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu, không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà 🥰. 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/Binhandaynay' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
